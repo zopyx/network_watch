9f0be128c9f8c2d5cb804c3729dd2c9eb0a181de
zopyx <yet@gmx.de>
2014-08-23 10:49:24 +0200

    updated

--- ping.py
+++ ping.py
@@ -11,15 +11,19 @@ def pinger(host, sleep=1):
 
         cmd = 'ping -c 1 -q {}'.format(host)
         status, output= subprocess.getstatusoutput(cmd)
-        print(output)
-        last_line = output.split('\n')[-1]
-        print(last_line)
-        time_field = last_line.split('=')
-        ms = time_field[1].split('/')[0].strip()
-        time.sleep(sleep)
-        status_line = '{} ; {} ; {}'.format(host, time.time(), ms)
+
+        if 'rtt min/avg/max/mdev' in output:
+            last_line = output.split('\n')[-1]
+            time_field = last_line.split('=')
+            ms = time_field[1].split('/')[0].strip()
+            time.sleep(sleep)
+            status_line = '{} ; {} ; {}'.format(host, time.time(), ms)
+        else:
+            status_line = '{} ; {} ; 9999'.format(host, time.time())
+
         fn = '{}.csv'.format(host)
         with open(fn, 'a') as fp:
+            print(status_line)
             fp.write(status_line + '\n')
 
 
